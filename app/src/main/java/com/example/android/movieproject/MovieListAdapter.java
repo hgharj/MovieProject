c57ad4558df3ec436b162ed40de625b0c91b801c
Jesse Gray <jgray.symm@gmail.com>
2018-08-13 07:14:17 -0400

    8/11/2018 Done 19:00

--- app/src/main/java/com/example/android/movieproject/MovieListAdapter.java
+++ app/src/main/java/com/example/android/movieproject/MovieListAdapter.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.android.movieproject.provider.MovieContract;
+
 /**
  * Created by jcgray on 8/5/18.
  */
@@ -24,12 +26,45 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
 
+        int movieListImageIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_MOVIE_POSTER);
+        int movieTitleIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_MOVIE_TITLE);
+        int releaseDateIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
+        int voteAverageIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE);
+        int voteDescIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_VOTE_DESCRIPTION);
+        int plotIndex = mCursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_PLOT);
+
+        int movieImageRes = mCursor.getInt(movieListImageIndex);
+        String movieTitle = mCursor.getString(movieTitleIndex);
+        long releaseDate = mCursor.getLong(releaseDateIndex);
+        double voteAverage = mCursor.getDouble(voteAverageIndex);
+        String voteDesc = mCursor.getString(voteDescIndex);
+        String plot = mCursor.getString(plotIndex);
+
+        /*int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow - createdAt, timeNow - wateredAt, plantType);
+
+        holder.plantImageView.setImageResource(imgRes);
+        holder.plantNameView.setText(String.valueOf(plantId));
+        holder.plantImageView.setTag(plantId);
+        */
+    }
+
+    public void swapCursor(Cursor newCursor) {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+        mCursor = newCursor;
+        if (mCursor != null) {
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (mCursor == null) return 0;
+        return mCursor.getCount();
     }
 
     @Override
